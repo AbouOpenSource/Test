101151abe04f27c116eb3f959e770f9f0b041378
AbouOpenSource <aboudra1996@gmail.com>
2020-01-28 12:19:38 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/projettest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projettest.dir/main.cpp.o"
+  "projettest.pdb"
+  "projettest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projettest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
